31f88bea20bad9f28081b801736fa64814db5c37
jimbobjork <t6bjji00@students.oamk.fi>
2018-11-16 12:17:25 +0200

    s

--- app/src/main/java/geocaching/app/CacheFoundActivity.java
+++ app/src/main/java/geocaching/app/CacheFoundActivity.java
@@ -1,23 +1,38 @@
 package geocaching.app;
 
 import android.app.Activity;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
-import android.nfc.NfcAdapter;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import java.nio.charset.Charset;
+public class CacheFoundActivity extends Activity {
+    private SharedPrefHelper sharedPrefHelper;
 
-public class CacheFoundActivity extends Activity{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cache_found);
+        sharedPrefHelper = new SharedPrefHelper(this);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.append(" " + sharedPrefHelper.getCacheSelection() + "!");
+
+        Button okButton = findViewById(R.id.button);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CacheFoundActivity.this, MainActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
     }
+
 }
